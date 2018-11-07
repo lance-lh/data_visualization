40640a4bc78147cd201d55d9bd63cc0c57c03bc6
haolee <imleehao@gmail.com>
2018-11-07 20:31:01 +0800

    missing data

--- highs_lows.py
+++ highs_lows.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -15,14 +15,17 @@ with open(filename) as f:
     highs = []
     lows = []
     for row in reader:
-        currrent_date = datetime.strptime(row[0],'%Y-%m-%d')
-        dates.append(currrent_date)
-
-        high = int(row[1])
-        highs.append(high)
-
-        low = int(row[3])
-        lows.append(low)
+        # error check
+        try:
+            currrent_date = datetime.strptime(row[0],'%Y-%m-%d')
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(currrent_date,'missing data')
+        else:
+            dates.append(currrent_date)
+            highs.append(high)
+            lows.append(low)
 
     # print(highs)
 
@@ -31,7 +34,7 @@ plt.plot(dates,highs,c='red',alpha=0.5)
 plt.plot(dates,lows,c='blue',alpha=0.5)
 plt.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
 
-plt.title("Daily high and low temperatures - 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014\nDeath Valley, CA", fontsize=24)
 plt.xlabel('',fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Tempetature (F)",fontsize=16)
