c0d4c77cd08eb1b7f283b1634b6cc81a688e9cc9
haolee <imleehao@gmail.com>
2018-11-07 20:23:19 +0800

    fill color between regions

--- highs_lows.py
+++ highs_lows.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -13,6 +13,7 @@ with open(filename) as f:
 
     dates = []
     highs = []
+    lows = []
     for row in reader:
         currrent_date = datetime.strptime(row[0],'%Y-%m-%d')
         dates.append(currrent_date)
@@ -20,14 +21,19 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
+        low = int(row[3])
+        lows.append(low)
+
     # print(highs)
 
 fig = plt.figure(dpi=128,figsize=(10,6))
-plt.plot(dates,highs,c='red')
+plt.plot(dates,highs,c='red',alpha=0.5)
+plt.plot(dates,lows,c='blue',alpha=0.5)
+plt.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
 
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel('',fontsize=16)
-fig.autofmt_xdate(rotation = 45)
+fig.autofmt_xdate()
 plt.ylabel("Tempetature (F)",fontsize=16)
 
 plt.tick_params(axis='both',which='major',labelsize=16)
