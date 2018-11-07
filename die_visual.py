a38aa14240afa06cd501148384b3f332139e9e59
haolee <imleehao@gmail.com>
2018-11-07 16:46:19 +0800

    roll D6 and D10 dice

--- die_visual.py
+++ die_visual.py
@@ -3,12 +3,12 @@ import pygal
 
 # create a D6
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # roll dice several times and store the results in a list
 results = []
 
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 # print(results)
@@ -23,12 +23,12 @@ for value in range(2,max_result+1):
 
 hist = pygal.Bar()
 
-hist.title = "Results of rolling two D6 dice 1000 times."
-hist.x_labels = ['2','3','4','5','6','7','8','9','10','11','12']
+hist.title = "Results of rolling a D6 and a D10 dice 50000 times."
+hist.x_labels = ['2','3','4','5','6','7','8','9','10','11','12','13','14','15','16']
 hist.x_titles = "Results"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6 + D6',frequencies)
-hist.render_to_file('die2_visual.svg')
+hist.add('D6 + D10',frequencies)
+hist.render_to_file('die2and10_visual.svg')
 
 
