b2c4a6157e863eed6bf5c6b9de62252e9d58c0fa
haolee <imleehao@gmail.com>
2018-11-08 09:40:43 +0800

    Pygal worldmap style

--- world_population.py
+++ world_population.py
@@ -1,6 +1,7 @@
 import json
 from country_codes import get_country_code
 import pygal.maps.world
+from pygal.style import RotateStyle as RS,LightColorizedStyle as LCS
 
 '''load data into a list'''
 filename = 'population_data.json'
@@ -31,9 +32,10 @@ for cc, pop in cc_populations.items():
     else:
         cc_pops_3[cc] = pop
 
-print(len(cc_pops_1),len(cc_pops_2),len(cc_pops_3))
+# print(len(cc_pops_1),len(cc_pops_2),len(cc_pops_3))
 
-wm = pygal.maps.world.World()
+wm_style = RS('#336699',base_style=LCS)
+wm = pygal.maps.world.World(style=wm_style)
 wm.title = 'World Population in 2010, by Country'
 wm.add('0-10m',cc_pops_1)
 wm.add('10m-1bn',cc_pops_2)
