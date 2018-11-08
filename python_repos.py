c2059e2579c98a69bdc00916dbaaed8ab9724c29
haolee <imleehao@gmail.com>
2018-11-08 14:40:38 +0800

    add link into chart

--- python_repos.py
+++ python_repos.py
@@ -36,10 +36,18 @@ print("Repositories returned:",len(repo_dicts)) # how many repos
 
 
 print("\nSelected information about each repository:")
-names, stars = [],[]
+names, plot_dicts = [],[]
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    # stars.append(repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value':repo_dict['stargazers_count'],
+        # 'label':repo_dict['description'],
+        'label': str(repo_dict['description']),
+        'xlink': repo_dict['html_url']
+        }
+    plot_dicts.append(plot_dict)
 
 # visualize
 my_style = LS('#333366',base_style=LCS)
@@ -58,5 +66,5 @@ chart = pygal.Bar(my_config,style=my_style)
 chart.title = 'Most-Starred Python Projects on Github'
 chart.x_labels = names
 
-chart.add('',stars)
+chart.add('',plot_dicts)
 chart.render_to_file('python_repos.svg')
